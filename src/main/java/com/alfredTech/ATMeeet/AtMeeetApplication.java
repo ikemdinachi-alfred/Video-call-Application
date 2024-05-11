cecc973dd411ff2ba2add4d3eb82d83261fec09e
ikemdinachi-alfred <ikemdinachialfred@gmail.com>
2024-05-11 08:39:43 +0100

    first commit

---
+++ src/main/java/com/alfredTech/ATMeeet/AtMeeetApplication.java
@@ -0,0 +1,13 @@
+package com.alfredTech.ATMeeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtMeeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtMeeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alfredTech/ATMeeet/AtMeeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfredTech.ATMeeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtMeeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
